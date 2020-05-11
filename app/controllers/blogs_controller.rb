481e369a19f7449b81f151473930a6e939685483
vagrant <vagrant@localhost.localdomain>
2020-05-11 16:53:46 +0000

    Fix showアクション % createアクション

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,8 +6,6 @@ class BlogsController < ApplicationController
 
   def show
   	@blog = Blog.find(params[:id])
-  	blog.save
-    redirect_to blog_path(blog.id)
   end
 
   def new
@@ -17,7 +15,7 @@ class BlogsController < ApplicationController
   def create
   	blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path
+    redirect_to blogs_path(blog.id)
   end
 
   def edit
